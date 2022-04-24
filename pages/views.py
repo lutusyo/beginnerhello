3004f2c68a7967ee343c4c2661ea62ec36a453e0
lutusyo <boni>
2022-04-24 12:19:37 +0300

    initial setup

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def homepageview(request):
+    return HttpResponse("Hello, world!")
